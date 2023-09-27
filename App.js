0006d1cf6c6a461d85333a97dcaf342be2425b1a
maria <maria.422020025@civitas.ukrida.ac.id>
2023-09-27 09:52:03 +0700

    splash screen, login, home

---
+++ App.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import FrameScreen from "./pages/FrameScreen";
+import LoginScreen from "./pages/Login"; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(false);
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "SpaceGrotesk-Regular": require("./assets/fonts/SpaceGrotesk-Regular.ttf"),
+  });
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="FrameScreen" component={FrameScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+
+};
+
+
+const Stack = createNativeStackNavigator();
+
+
+export default App;
+
---
+++ GlobalStyles.js
@@ -0,0 +1,79 @@
+/* fonts */
+export const FontFamily = {
+    poppinsBold: "Poppins-Bold",
+    poppinsMedium: "Poppins-Medium",
+    poppinsRegular: "Poppins-Regular",
+    poppinsLight: "Poppins-Light",
+    poppinsSemiBold: "Poppins-SemiBold",
+    robotoMedium: "Roboto-Medium",
+    spaceGroteskRegular: "SpaceGrotesk-Regular",
+    poppinsBlack: "Poppins-Black",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_xl: 20,
+    size_3xs_5: 10,
+    size_xs: 12,
+    size_smi: 13,
+    size_lg_1: 18,
+    size_sm: 14,
+    size_3xs: 10,
+    size_3xs_2: 9,
+    size_4xs_3: 8,
+    size_base: 16,
+    size_4xs_8: 9,
+    size_2xs_6: 11,
+    size_5xs: 8,
+    size_8xs_4: 4,
+    size_10xs_7: 3,
+    size_8xs_9: 5,
+    size_4xs: 9,
+  };
+  /* Colors */
+  export const Color = {
+    white: "#fff",
+    tomato: "#fc4b41",
+    blue2: "#2d9cdb",
+    darkslateblue: "#11336d",
+    gray_100: "#fcfcfc",
+    gray_200: "#8c968c",
+    gray_300: "#211b3d",
+    wireframesColorsTextLegibilityHighEmphasis: "#170e2b",
+    gray_400: "rgba(0, 0, 0, 0.06)",
+    dimgray_100: "#6c6c6c",
+    dimgray_200: "#65676d",
+    black: "#000",
+    whitesmoke_100: "#f6f6f6",
+    colorsBackgroundsLight: "#f5f5f5",
+    midnightblue: "#241856",
+    s: "#59597c",
+    secondaryGrey: "#8593a8",
+    f9FB9: "#88879c",
+    whiteInput: "#f6fbff",
+    darkslategray_100: "#343434",
+    amberBlack: "#072d4b",
+  };
+  /* Paddings */
+  export const Padding = {
+    p_170xl: 189,
+    p_xl: 20,
+    p_5xl: 24,
+    p_base: 16,
+    p_xs: 12,
+    p_sm: 14,
+    p_mini: 15,
+  };
+  /* border radiuses */
+  export const Border = {
+    br_xl: 20,
+    br_xs: 12,
+    br_59xl_6: 79,
+    br_3xs_9: 10,
+    br_31xl: 50,
+    br_5xs_4: 7,
+    br_xs_9: 12,
+    br_3xs: 10,
+    br_sm_2: 13,
+    br_9xs_8: 4,
+    br_69xl: 88,
+  };
