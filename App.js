0bf48d63283573b6358d3c1d3d5ea393ddcf791f
mseveline <evelinekurniadharma@gmail.com>
2023-10-11 12:00:48 +0800

    update app.js

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import * as React from "react";
+import React, { forwardRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import FrameScreen from "./pages/FrameScreen";
 import LoginScreen from "./pages/Login"; 
+import CallMyChild from "./pages/CallMyChild";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View, Text, Pressable, TouchableOpacity } from "react-native";
+import Toast from "react-native-toast-message"; 
 
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   const [hideSplashScreen, setHideSplashScreen] = React.useState(false);
@@ -20,20 +24,44 @@ const App = () => {
     "SpaceGrotesk-Regular": require("./assets/fonts/SpaceGrotesk-Regular.ttf"),
   });
 
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="CallMyChild"
+          screenOptions={{ headerShown: false }}
+        >
+      
+          <Stack.Screen
+            name="CallMyChild"
+            component={CallMyChild}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="FrameScreen" component={FrameScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
 
+      <Toast ref={(ref) => Toast.setRef(ref)} />
+
+      {/* Your Toast component */}
+      <Toast
+        ref={(ref) => Toast.setRef(ref)}
+        text1="Successfully, call your child!"
+        text1Style={{ fontSize: 15 }}
+        text2Style={{ fontSize: 13 }}
+      />
+    </>
+  );
 };
 
-
-const Stack = createNativeStackNavigator();
-
-
 export default App;
-
