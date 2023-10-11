791866eb4a0e463e7f35d6661d9db7e894f9a2eb
Andeist <114631553+Andeist@users.noreply.github.com>
2023-10-11 20:34:43 +0800

    Added Dasboard and ReportCard pages
    Added Dasboard and ReportCard pages

---
+++ components/Attendance.js
@@ -0,0 +1,182 @@
+import * as React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { Image } from "expo-image";
+import { FontSize, FontFamily, Color, Border } from "../GlobalStyles";
+
+const Attendance = ({ onClose }) => {
+  return (
+    <View style={styles.attendance}>
+      <View style={styles.mensahParent}>
+        <View style={styles.mensah}>
+          <View style={styles.ellipseParent}>
+            <Image
+              style={styles.frameChild}
+              contentFit="cover"
+              source={require("../assets/images/ellipse-91.png")}
+            />
+            <Text style={styles.text}>100%</Text>
+          </View>
+          <Text style={styles.mintaDonor}>ATTENDANCE</Text>
+        </View>
+        <View style={styles.maskGroup}>
+          <View style={styles.frameParent}>
+            <View style={styles.frame}>
+              <View style={styles.frameItem} />
+              <Text style={[styles.mintaDonor1, styles.mintaTypo]}>
+                0 Absent
+              </Text>
+            </View>
+            <View style={[styles.frame1, styles.frameFlexBox]}>
+              <View style={styles.frameInner} />
+              <Text style={[styles.mintaDonor2, styles.mintaTypo]}>
+                18 Present
+              </Text>
+            </View>
+            <View style={[styles.frame2, styles.frameFlexBox]}>
+              <Image
+                style={styles.rectangleIcon}
+                contentFit="cover"
+                source={require("../assets/images/rectangle-367.png")}
+              />
+              <Text style={[styles.mintaDonor1, styles.mintaTypo]}>
+                2 Excused
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mintaTypo: {
+    marginLeft: 8,
+    width: 72,
+    fontSize: FontSize.size_3xs,
+    fontFamily: FontFamily.poppinsRegular,
+    letterSpacing: 0,
+    textAlign: "center",
+    color: Color.colorMidnightblue,
+  },
+  frameFlexBox: {
+    marginTop: 4,
+    overflow: "hidden",
+    width: 120,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  frameChild: {
+    borderRadius: Border.br_21xl,
+    height: 120,
+    zIndex: 0,
+    width: 120,
+  },
+  text: {
+    position: "absolute",
+    top: 46,
+    left: 28,
+    fontSize: 25,
+    lineHeight: 28,
+    fontWeight: "700",
+    fontFamily: FontFamily.poppinsBold,
+    zIndex: 1,
+    textAlign: "center",
+    color: Color.colorMidnightblue,
+  },
+  ellipseParent: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mintaDonor: {
+    fontSize: FontSize.size_mini,
+    marginTop: 12,
+    fontFamily: FontFamily.poppinsRegular,
+    letterSpacing: 0,
+    textAlign: "center",
+    color: Color.colorMidnightblue,
+  },
+  mensah: {
+    paddingLeft: 50,
+    paddingTop: 23,
+    paddingRight: 54,
+    paddingBottom: 28,
+    justifyContent: "center",
+    shadowOpacity: 1,
+    elevation: 35.6,
+    shadowRadius: 35.6,
+    shadowOffset: {
+      width: 0,
+      height: 5.93411922454834,
+    },
+    shadowColor: "rgba(0, 0, 0, 0.06)",
+    backgroundColor: Color.colorWhite,
+    borderRadius: 12,
+    alignSelf: "stretch",
+    alignItems: "center",
+  },
+  frameItem: {
+    backgroundColor: Color.colorDarkslateblue,
+    width: 40,
+    height: 17,
+  },
+  mintaDonor1: {
+    height: 14,
+  },
+  frame: {
+    overflow: "hidden",
+    height: 17,
+    width: 120,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  frameInner: {
+    backgroundColor: Color.colorTomato,
+    height: 18,
+    width: 40,
+  },
+  mintaDonor2: {
+    height: 12,
+  },
+  frame1: {
+    height: 18,
+  },
+  rectangleIcon: {
+    width: 40,
+    height: 17,
+  },
+  frame2: {
+    height: 17,
+  },
+  frameParent: {
+    alignItems: "center",
+  },
+  maskGroup: {
+    width: 154,
+    paddingHorizontal: 22,
+    paddingVertical: 15,
+    marginTop: 30,
+    shadowOpacity: 1,
+    elevation: 35.6,
+    shadowRadius: 35.6,
+    shadowOffset: {
+      width: 0,
+      height: 5.93411922454834,
+    },
+    shadowColor: "rgba(0, 0, 0, 0.06)",
+    backgroundColor: Color.colorWhite,
+    borderRadius: 12,
+  },
+  mensahParent: {
+    alignSelf: "stretch",
+    alignItems: "center",
+  },
+  attendance: {
+    maxWidth: "100%",
+    maxHeight: "100%",
+    alignItems: "center",
+  },
+});
+
+export default Attendance;
---
+++ components/CaraouselOption3.js
@@ -0,0 +1,140 @@
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { FontSize, FontFamily, Color, Border, Padding } from "../GlobalStyles";
+
+const CaraouselOption3 = ({ onClose }) => {
+  return (
+    <View style={styles.caraouselOption3}>
+      <View style={[styles.newsFrame, styles.frameFlexBox1]}>
+        <View style={[styles.frameParent, styles.frameFlexBox1]}>
+          <View style={[styles.frame, styles.frameFlexBox]}>
+            <Text style={styles.merdeka}>Merdeka!</Text>
+          </View>
+          <View style={[styles.frame1, styles.frameSpaceBlock]}>
+            <Image
+              style={styles.frameChild}
+              contentFit="cover"
+              source={require("../assets/images/rectangle-162.png")}
+            />
+          </View>
+          <View style={[styles.frame2, styles.frameSpaceBlock]}>
+            <View style={[styles.frame3, styles.frameFlexBox]}>
+              <Text style={styles.topAchieverOf}>
+                Top Achiever of grade eleven online in the fourth week! Keep up
+                the good work everyone! Stay tuned for next week's ranking
+              </Text>
+            </View>
+            <View style={styles.frame4}>
+              <View style={styles.frame5}>
+                <Text style={styles.newsTypo}>News</Text>
+                <Text
+                  style={[styles.aug2023, styles.newsTypo]}
+                >{`13, Aug 2023 `}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  frameFlexBox1: {
+    flex: 1,
+    alignSelf: "stretch",
+  },
+  frameFlexBox: {
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  frameSpaceBlock: {
+    marginTop: 18,
+    overflow: "hidden",
+  },
+  newsTypo: {
+    opacity: 0.4,
+    fontSize: FontSize.size_mini,
+    textAlign: "left",
+    fontFamily: FontFamily.poppinsRegular,
+    lineHeight: 17,
+    color: Color.amberBlack,
+  },
+  merdeka: {
+    fontSize: FontSize.size_base,
+    fontWeight: "600",
+    fontFamily: FontFamily.poppinsSemiBold,
+    textAlign: "center",
+    color: Color.amberBlack,
+    flex: 1,
+    alignSelf: "stretch",
+  },
+  frame: {
+    overflow: "hidden",
+    flex: 1,
+    alignSelf: "stretch",
+  },
+  frameChild: {
+    borderRadius: 3,
+    width: 170,
+    height: 330,
+  },
+  frame1: {
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  topAchieverOf: {
+    fontSize: FontSize.size_mid,
+    width: 273,
+    fontFamily: FontFamily.poppinsRegular,
+    lineHeight: 20,
+    color: Color.amberBlack,
+  },
+  frame3: {
+    overflow: "hidden",
+  },
+  aug2023: {
+    width: 118,
+    marginLeft: 10,
+  },
+  frame5: {
+    flexDirection: "row",
+    overflow: "hidden",
+  },
+  frame4: {
+    marginTop: 6,
+    overflow: "hidden",
+    justifyContent: "center",
+  },
+  frame2: {
+    alignItems: "center",
+  },
+  frameParent: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  newsFrame: {
+    shadowColor: "rgba(0, 0, 0, 0.5)",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowRadius: 2,
+    elevation: 2,
+    shadowOpacity: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  caraouselOption3: {
+    borderRadius: Border.br_xs,
+    backgroundColor: Color.colorWhite,
+    width: 300,
+    height: 584,
+    padding: Padding.p_3xs,
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+});
+
+export default CaraouselOption3;
