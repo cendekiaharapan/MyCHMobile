946e4e1d60591efbe825ea20b6d648dfc9677315
Cendekia harapan <142763175+cendekiaharapan@users.noreply.github.com>
2023-11-16 07:53:55 +0800

    changed name header term

--- pages/AssessmentDetail.js
+++ pages/AssessmentDetail.js
@@ -14,11 +14,11 @@ import { useNavigation } from "@react-navigation/native";
 import { Color, FontFamily, FontSize, Border, Padding } from "../GlobalStyles";
 // import React, { useState } from "react";
 import AsessmentComponent from "../components/AsessmentComponent";
-import Header from "../components/Header";
+import Header from "../components/HeaderTerm";
 import Criteria from "../components/Criteria";
 
-const AssessmentDetail = ({route}) => {
-    const assessmentData = route.params.subject;
+const AssessmentDetail = ({ route }) => {
+  const assessmentData = route.params.subject;
 
   return (
     <NativeBaseProvider>
@@ -40,83 +40,87 @@ const AssessmentDetail = ({route}) => {
           </View>
 
           {assessmentData && (
-            <Header assessmentData={assessmentData} title={assessmentData.title} score={assessmentData.avg_score} />
+            <Header
+              assessmentData={assessmentData}
+              title={assessmentData.title}
+              score={assessmentData.avg_score}
+            />
           )}
 
           <View style={styles.mainContainer}>
             <View style={styles.container}>
-              <View style={[styles.column, styles.marginRight, styles.marginBottom]}>
-              <AsessmentComponent
-            Text_Criteria="Booklet"
-            score={assessmentData?.booklet}
-          />
-          <AsessmentComponent
-            Text_Criteria="Subject Integration"
-            score={assessmentData?.subject_integration}
-          />
-          <AsessmentComponent
-            Text_Criteria="Progression Test"
-            score={assessmentData?.progress_test}
-          />
-               <AsessmentComponent
-            Text_Criteria="Final Block Assessment"
-            score={assessmentData?.final_block_assessment}
-          />
-          
-        </View>
+              <View
+                style={[styles.column, styles.marginRight, styles.marginBottom]}
+              >
+                <AsessmentComponent
+                  Text_Criteria="Booklet"
+                  score={assessmentData?.booklet}
+                />
+                <AsessmentComponent
+                  Text_Criteria="Subject Integration"
+                  score={assessmentData?.subject_integration}
+                />
+                <AsessmentComponent
+                  Text_Criteria="Progression Test"
+                  score={assessmentData?.progress_test}
+                />
+                <AsessmentComponent
+                  Text_Criteria="Final Block Assessment"
+                  score={assessmentData?.final_block_assessment}
+                />
+              </View>
               <View style={[styles.column, styles.marginBottom]}>
-              <AsessmentComponent
-            Text_Criteria="Presentation"
-            score={assessmentData?.presentation}
-          />
-          <AsessmentComponent
-            Text_Criteria="Mock Test"
-            score={assessmentData?.mock_test}
-          />
-          <AsessmentComponent
-            Text_Criteria="Average Daily Score"
-            score={assessmentData?.avg_score}
-          />
-            <AsessmentComponent
-            Text_Criteria="Mid Block Assessment"
-            score={assessmentData?.mid_block_assessment}
-          />
-     
-        </View></View>
+                <AsessmentComponent
+                  Text_Criteria="Presentation"
+                  score={assessmentData?.presentation}
+                />
+                <AsessmentComponent
+                  Text_Criteria="Mock Test"
+                  score={assessmentData?.mock_test}
+                />
+                <AsessmentComponent
+                  Text_Criteria="Average Daily Score"
+                  score={assessmentData?.avg_score}
+                />
+                <AsessmentComponent
+                  Text_Criteria="Mid Block Assessment"
+                  score={assessmentData?.mid_block_assessment}
+                />
+              </View>
             </View>
           </View>
         </View>
+      </View>
     </NativeBaseProvider>
   );
 };
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-      },
-      container: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginLeft: 10,
-        marginRight: -5,
-        alignContent: "center",
-        justifyContent: "center",
-      },
-      column: {
-        flex: 1,
-        paddingVertical: 10,
-        
-      },
-      marginRight: {
-        marginRight: 20,
-        alignContent: "center",
-        justifyContent: "center",
-    },
-    marginBottom: {
-       paddingBottom: 50,
-       paddingRight: 70,
-       alignContent: "center",
-    },
+  mainContainer: {
+    flex: 1,
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginLeft: 10,
+    marginRight: -5,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  column: {
+    flex: 1,
+    paddingVertical: 10,
+  },
+  marginRight: {
+    marginRight: 20,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  marginBottom: {
+    paddingBottom: 50,
+    paddingRight: 70,
+    alignContent: "center",
+  },
 
   bodyContainer: {
     flex: 0.53,
@@ -124,7 +128,7 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     marginRight: 15,
     alignContent: "center",
-        justifyContent: "center",
+    justifyContent: "center",
   },
   headerContainer: {
     flex: 0.1,
